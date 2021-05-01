96892476afcb4ce22e6ce26d9b408e2abe3bdd14
kyoowon <kyoowon95@gmail.com>
2021-05-01 16:17:33 +0900

    #1 raycasting_tutorial mlx_new_window

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: kyuwonlee <kyuwonlee@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/28 16:06:30 by kyuwonlee         #+#    #+#             */
-/*   Updated: 2021/05/01 15:49:58 by kyuwonlee        ###   ########.fr       */
+/*   Updated: 2021/05/01 16:15:39 by kyuwonlee        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,28 @@ typedef struct s_info
 	double dirY;
 	double planeX;
 	double planeY;
+    double	moveSpeed;
+	double	rotSpeed;
 }               t_info;
 
+typedef struct s_map
+{
+    int width;
+    int height;
+    int *mapptr;
+}               t_map;
 
 int        main(void)
 {
     t_info info;
 
+
     info.height = 500;
     info.width = 700;
+    
 
 	info.mlx = mlx_init();
+
     info.win = mlx_new_window(info.mlx, info.width, info.height, "kyulee");
     mlx_loop(info.mlx);
     return (0);
